ce8356b9663407ee1d8307f6b9b3e34221f98b04
Vu Tuan Minh <vutuanminhvtm2k5@gmail.com>
2025-07-02 15:55:53 +0700

    3:55 2/7/25

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProjectIntro from "../components/ProjectIntro";
+
+const Home = () => (
+    <main>
+        <ProjectIntro />
+        {/* Các section khác */}
+    </main>
+);
+
+export default Home;
