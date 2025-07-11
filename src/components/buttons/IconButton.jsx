1a05380c14a957df897aa0a8cbf66a53fc8eed78
Vu Tuan Minh <vutuanminhvtm2k5@gmail.com>
2025-07-12 00:33:04 +0700

    section 2 and 3

--- src/components/buttons/IconButton.jsx
+++ src/components/buttons/IconButton.jsx
@@ -1,10 +1,9 @@
 import buttonSvg from "../../assets/Subtract.svg";
 import './Button.css'
 
-export default function IconButton({ href, label }) {
+export default function IconButton({ href, label, className }) {
     return (
-        <a href={href} className="icon-button">
-            {/*<img src={buttonSvg} alt="" className="shape" />*/}
+        <a href={href} className={`icon-button ${className}`}>
             <span className="label">{label}</span>
         </a>
     );
